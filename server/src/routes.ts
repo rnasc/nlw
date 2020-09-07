110647139d402637bcb401109485175b539e3167
Rogerio Nascimento <rogerio@rogerionascimento.com>
2020-09-07 16:02:48 +0100

    initial commit

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express, { response } from 'express'
+import PointsController from './controllers/points_controller'
+import ItemsController from './controllers/items_controller'
+
+const routes = express.Router()
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
